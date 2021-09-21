3c99a1c3a1ad92f31c0bd59e8c8e8169d5f84c66
Vaughn Wenner <vdwenner@gmail.com>
2021-09-21 08:15:16 -0400

    All Command-line applications finished

--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,13 @@
 package com.techelevator;
+import java.util.Scanner;
 
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Decimal number that you want converted to Binary: ");
+		int decimal = sc.nextInt();
+		System.out.println(Integer.toBinaryString(decimal));
 
 	}
 
--- module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise-student/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,48 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class Fibonacci {
-
 	public static void main(String[] args) {
+		//create scanner
+		Scanner sc = new Scanner(System.in);
 
+//Banner
+		System.out.println("****************");
+		System.out.println("Fibonacci Finder");
+		System.out.println("****************");
+
+//user input
+		System.out.print("Enter whole number: ");
+		int userInput = Integer.parseInt(sc.nextLine());
+
+//if else to handle exceptions
+		if (userInput <= 0) {
+			System.out.println("0, 1");
+		} else {
+			System.out.print("0, 1, 1, ");
+		}
+
+//
+		int f2 = 1;
+		int fibonnacci = 1;
+
+		for (int i = 1; i + f2 < userInput; ) {
+
+			fibonnacci = i + f2;
+
+			i = f2;
+
+			f2 = fibonnacci;
+
+			if (fibonnacci + i <= userInput) {
+				System.out.print(fibonnacci + ", ");
+			} else {
+				System.out.print(fibonnacci);
+			}
+			if (userInput == i + f2) {
+				System.out.print(fibonnacci + i);
+
+			}
+
+		}
 	}
-
 }
