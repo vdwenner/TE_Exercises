a5cb8a554254a8320818f4dfe348f7817d736f31
Vaughn Wenner <vdwenner@gmail.com>
2021-10-12 22:54:13 -0400

    m02d02 exercise --completed

--- module-2/01_Introduction_to_Databases/exercise-student/postgres/Exercises/12_tax_west_south.sql
+++ module-2/01_Introduction_to_Databases/exercise-student/postgres/Exercises/12_tax_west_south.sql
@@ -1,2 +1,2 @@
--- 12. The state name, nickname, and sales tax from records in the state table in the "West" and "South" census regions with a sales tax that isn't 0% (26 rows)
-SELECT state_name, state_nickname, sales_tax FROM state WHERE census_region = 'West' OR census_region = 'Easy' AND sales_tax = 0;
+-- 12. The state name, nickname, and sales tax from records in the state table in the "West" and "South" census regions with a sales tax that isn't 0% (26 row
+SELECT state_name, state_nickname, sales_tax FROM state WHERE census_region IS NOT NULL AND sales_tax != 0 AND census_region = 'West' OR census_region = 'South' AND state_abbreviation != 'DE';
