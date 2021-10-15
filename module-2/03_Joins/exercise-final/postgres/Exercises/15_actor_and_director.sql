0b693b2eb0e797f247cc0011a414e9705395e844
Rich Seeds <rich@techelevator.com>
2021-10-15 12:25:29 -0400

    Mod 2 Day 5 Review Student Exercise, Day 3 Exercise Final

---
+++ module-2/03_Joins/exercise-final/postgres/Exercises/15_actor_and_director.sql
@@ -0,0 +1,6 @@
+-- 15. The title of the movie and the name of director for movies where the director was also an actor in the same movie (73 rows)
+
+SELECT m.title, person_name
+FROM person p
+JOIN movie_actor ma ON p.person_id = ma.actor_id
+JOIN movie m ON ma.movie_id = m.movie_id AND p.person_id = m.director_id;
