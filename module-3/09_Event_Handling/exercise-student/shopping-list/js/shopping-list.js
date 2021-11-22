d7fe722fd7acf1e17eef2e92e42d7bea5539ba2c
Vaughn Wenner <vdwenner@gmail.com>
2021-11-22 17:28:39 -0500

    m03d09 --exercises completed

--- module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
@@ -1,16 +1,16 @@
 let allItemsIncomplete = true;
 const pageTitle = 'My Shopping List';
 const groceries = [
-  { id: 1, name: 'Oatmeal', completed: false },
+  { id: 1, name: 'Eggs', completed: false },
   { id: 2, name: 'Milk', completed: false },
   { id: 3, name: 'Banana', completed: false },
-  { id: 4, name: 'Strawberries', completed: false },
-  { id: 5, name: 'Lunch Meat', completed: false },
+  { id: 4, name: 'Bacon', completed: false },
+  { id: 5, name: 'Turkey', completed: false },
   { id: 6, name: 'Bread', completed: false },
   { id: 7, name: 'Grapes', completed: false },
   { id: 8, name: 'Steak', completed: false },
   { id: 9, name: 'Salad', completed: false },
-  { id: 10, name: 'Tea', completed: false }
+  { id: 10, name: 'Coffee', completed: false }
 ];
 
 /**
@@ -37,5 +37,58 @@ function displayGroceries() {
   });
 }
 
-setPageTitle();
-displayGroceries();
+
+document.addEventListener('DOMContentLoaded', () => {
+  setPageTitle();
+  displayGroceries();
+
+  const items = document.querySelectorAll('li');
+  items.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      complete(event.currentTarget);
+    });
+
+    item.addEventListener('dblclick', (event) => {
+      incomplete(event.currentTarget);
+    });
+  });
+
+  const btnToggleAll = document.getElementById('toggleAll');
+  btnToggleAll.addEventListener('click', (event) => {
+    toggleAll(items, event.currentTarget);
+  });
+});
+
+function complete(item) {
+  if (!item.classList.contains('completed')) {
+    item.classList.add('completed');
+    item.querySelector('i').classList.add('completed');
+  }
+}
+
+function incomplete(item) {
+  if (item.classList.contains('completed')) {
+    item.classList.remove('completed');
+    item.querySelector('i').classList.remove('completed');
+  }
+}
+
+function toggleAll(items, btn) {
+  items.forEach((item) => {
+    addRemoveCompleted(item);
+  });
+  allItemsIncomplete = !allItemsIncomplete;
+  allItemsIncomplete == true
+    ? (btn.innerText = 'Mark All Complete')
+    : (btn.innerText = 'Mark All Incomplete');
+}
+
+function addRemoveCompleted(item) {
+  if (allItemsIncomplete) {
+    item.classList.add('completed');
+    item.querySelector('i').classList.add('completed');
+  } else {
+    item.classList.remove('completed');
+    item.querySelector('i').classList.remove('completed');
+  }
+}
---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/lecture-student/vue-product-review/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-review></product-review>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import ProductReview from './components/ProductReview.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    ProductReview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
